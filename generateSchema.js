50bb566aa57913edcad9e759968b151b54211d4f
CJ <carljohan.kihl@gmail.com>
2020-01-13 17:16:34 +0100

    Initial Commit

---
+++ generateSchema.js
@@ -0,0 +1,25 @@
+const { GraphQLClient } = require("graphql-request");
+const { generateTypeScriptTypes } = require("graphql-schema-typescript");
+const { buildClientSchema } = require("graphql/utilities/buildClientSchema");
+const { introspectionQuery } = require("graphql/utilities/introspectionQuery");
+require("dotenv").config();
+
+const graphQLClient = new GraphQLClient(process.env.STOREFRONT_ENDPOINT, {
+  headers: {
+    "X-Shopify-Storefront-Access-Token": process.env.ACCESS_TOKEN
+  }
+});
+
+graphQLClient.request(introspectionQuery).then(data => {
+  const builded = buildClientSchema(data);
+  console.log(builded);
+  generateTypeScriptTypes(builded, "./index.d.ts", { typePrefix: "" })
+    .then(() => {
+      console.log("DONE");
+      process.exit(0);
+    })
+    .catch(err => {
+      console.error(err);
+      process.exit(1);
+    });
+});
